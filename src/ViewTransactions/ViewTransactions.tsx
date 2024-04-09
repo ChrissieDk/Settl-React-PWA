fe0cd3727113a3491ac05400a7be69bc7248ba27
Christiaan de Kock <christiaan@getsavvi.co.za>
2024-04-09 10:22:39 +0200

    Small ui update

--- src/ViewTransactions/ViewTransactions.tsx
+++ src/ViewTransactions/ViewTransactions.tsx
@@ -32,8 +32,8 @@ const ViewTransactions: React.FC<TransactionModalProps> = ({
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-30 z-50 flex justify-center items-center">
-      <div className="bg-white p-5 rounded-lg max-w-3xl w-full max-h-[80vh] overflow-hidden shadow">
+    <div className="fixed inset-0 z-50 flex justify-center items-center">
+      <div className="bg-white p-5 rounded-lg max-w-3xl w-full max-h-[80vh] overflow-hidden shadow mx-2">
         <div className="flex justify-between items-center mb-4 border-b pb-2">
           <h2 className="text-2xl font-semibold">Detailed Transactions</h2>
           <button onClick={onClose} className="text-lg font-semibold">
